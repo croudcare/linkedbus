6963386797756f40473dc9fb55b2063bc73f6dbe
Thiago Dantas <thiago.teixeira.dantas@gmail.com>
2014-03-04 13:46:02 +0000

    mispelling pid spec feature

--- spec/pid_spec.rb
+++ spec/pid_spec.rb
@@ -10,7 +10,7 @@ describe LinkedBus::Pid do
     FileUtils.rm_rf(Pathname.new(path).dirname)
   end
 
-  it "Write the pid in the specified file" do
+  it "writes the pid in the specified file" do
     LinkedBus::Pid.write(path)
     expect(File.read(path)).to match(/#{Process.pid}/)
   end
