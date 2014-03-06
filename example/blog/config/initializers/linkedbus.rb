538e409fa42b9408aa7cc4e8590de5c6872bd8df
Thiago Dantas <thiago.teixeira.dantas@gmail.com>
2014-03-06 22:58:13 +0000

    rails blog example change

--- example/blog/config/initializers/linkedbus.rb
+++ example/blog/config/initializers/linkedbus.rb
@@ -1,3 +1,7 @@
-LinkedBus::Subscribers.register do |manager|
-  manager.subscribe "blog.signin",  "signin_queue", Subscribers::CommentHandler 
+LinkedBus::Exchanges.register do |exchange|
+
+  exchange.topic('blog_exchange') do |topic|
+    topic.subscribe "blog.signin",  "signin_queue", Subscribers::CommentHandler 
+  end
+
 end
